325ab44fa413bbe86a3f3f18f733cd022ccc76ca
Carlos Vazquez <carlosangel2710@hotmail.com>
2025-11-08 14:11:49 -0600

    rutas

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def landing_page():
+    return render_template('signup.html')
+
+@app.route('/app/static/admin/')
+def api_status():
+    data = {
+        "status": "ok",
+        "messange": "El servidor de la API está funcionando"
+    }
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
