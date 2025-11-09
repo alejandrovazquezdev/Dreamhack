3f4759d7fb822dbbd92ae9381925de3663b43e25
regina <regival3307@gmail.com>
2025-11-08 18:21:54 -0600

    ale correcto

---
+++ db.py
@@ -0,0 +1,4 @@
+from flask_sqlalchemy import SQLAlchemy
+
+#Crear el objeto de la base de datos de tipo SQLAlchemy
+db = SQLAlchemy()
---
+++ models.py
@@ -0,0 +1,24 @@
+from app import db
+from sqlalchemy.orm import Mapped, mapped_column
+
+#Modelo de datos
+
+class Usuarios(db.Model):
+    #campos
+    # id = db.Column(db.Integer, primary_key=True) #Antes se utilizaba así
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column()
+    lastanme: Mapped[str] = mapped_column()
+    lastname2: Mapped[str] = mapped_column()
+    email: Mapped[str] = mapped_column(unique=True)
+    
+    #metodo str para devolver los metodos de la class
+    def __str__(self):
+        return (
+            f'id:{self.id}'
+            f'nombre(s){self.name}'
+            f'apellido Materno{self.lastanme}'
+            f'apellido Paterno{self.lastname2}'
+            f'correo Electrónico{self.email}'
+        )
